48cc4c9b7a01cbe00e3411c871a35129fa1b78b4
Phong Black <80878082+PhongBlack@users.noreply.github.com>
2021-05-08 06:59:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,11 +5,10 @@ if test $(which git); then
   cd mlmampro/install
   pwd
   sh 1.brew.sh
-  sh 2.ohmyzsh.sh
-  sh 3.zsh-config.sh
-  sh 4.apache.sh
-  sh 5.php.sh
-  sh 6.mariadb.sh
+  sh 2.apache.sh
+  sh 3.php.sh
+  sh 4.mariadb.sh
+  
 else
   echo "Please complete the Command Line Tools installation first by running this command in Terminal: git"
 fi
