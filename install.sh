f7a7753b9e3d18c28107ac8108a2bc6021642015
Phong Black <80878082+PhongBlack@users.noreply.github.com>
2021-05-08 23:30:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 if test $(which git); then
   cd ~
   git clone https://github.com/phongblack/mlmampro.git ~/mlmampro
-  chmod +x ~/mlmampro/install/*
-  ~/mlmampro/install/1.brew.sh
-  ~/mlmampro/install/2.apache.sh
-  ~/mlmampro/install/3.php.sh
-  ~/mlmampro/install/4.mariadb.sh
-  ~/mlmampro/install/5.ohmyzsh.sh
-  /usr/bin/zsh compaudit | xargs chmod g-w
+  
+  sh ~/mlmampro/install/1.brew.sh
+  sh ~/mlmampro/install/2.apache.sh
+  sh ~/mlmampro/install/3.php.sh
+  sh ~/mlmampro/install/4.mariadb.sh
+  sh ~/mlmampro/install/5.ohmyzsh.sh
+  
   
 else
   echo "Please complete the Command Line Tools installation first by running this command in Terminal: git"
